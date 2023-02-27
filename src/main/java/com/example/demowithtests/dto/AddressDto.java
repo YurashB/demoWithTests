6e9669fd7d03212125037489e83877ee8cdd3065
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-02-27 13:29:31 +0200

    Fix building and startup of project

--- src/main/java/com/example/demowithtests/dto/AddressDto.java
+++ src/main/java/com/example/demowithtests/dto/AddressDto.java
@@ -1,11 +1,9 @@
 package com.example.demowithtests.dto;
 
-import lombok.experimental.Accessors;
 
 import java.time.Instant;
 import java.util.Date;
 
-//@Accessors(chain = true)
 public class AddressDto {
 
     public Long id;
@@ -17,7 +15,4 @@ public class AddressDto {
     public String city;
 
     public String street;
-
-    //todo: dfhgjkdfhg Jira - 5544
-    public Date date = Date.from(Instant.now());
 }
